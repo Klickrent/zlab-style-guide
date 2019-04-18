956ac2b4ad43167cc1cdf39517427efbc5e56bfe
Matthias Sommerfeld <matthias.sommerfeld@algo26.de>
2019-04-18 15:51:46 +0200

    Integrate Bootstrap

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,2 @@
 require('../../node_modules/bootstrap-material-design/dist/js/bootstrap-material-design.js');
-require('../../node_modules/bootstrap-material-design/dist/css/bootstrap-material-design.css');
 require('../scss/main.scss');

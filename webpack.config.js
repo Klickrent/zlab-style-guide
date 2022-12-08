5fb1385d73c13a37b1b41e741b5520b1bd283be3
kayes <kayes@live.de>
2022-12-08 12:11:29 +0100

    Generate new zamicon font

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
-const plugins = [ new MiniCssExtractPlugin({ filename: 'css/main.css' }) ];
 
 module.exports = {
     mode: 'production',
@@ -11,7 +10,7 @@ module.exports = {
     },
     resolve: { alias: { 'jquery': 'jquery/dist/jquery.slim.js' } },
     watchOptions: { poll: 1000 },
-    plugins: plugins,
+    plugins: [ new MiniCssExtractPlugin({ filename: 'css/main.css' }) ],
     module: {
         rules: [
             {
